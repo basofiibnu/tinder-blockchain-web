7c61c79f6dad65307d8ac1c02b48a5f249c24e7b
basofiibnu <>
2022-03-26 10:41:20 +0700

    added header aand card component

---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Header from '../components/Header'
+import Card from '../components/Card'
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col bg-[#222229]`,
+  cardsContainer: `flex flex-col items-center justify-center flex-1`,
+}
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={style.wrapper}>
+        <Header />
+        <div className={style.cardsContainer}>
+          <Card />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
