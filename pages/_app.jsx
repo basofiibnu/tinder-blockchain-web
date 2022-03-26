27b5eaa866566bd386dda69e4ee42a62711323d3
basofiibnu <>
2022-03-26 10:47:48 +0700

    setup contex

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import { TinderProvider } from '../context/TinderContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <TinderProvider>
+      <Component {...pageProps} />
+    </TinderProvider>
+  )
+}
+
+export default MyApp
